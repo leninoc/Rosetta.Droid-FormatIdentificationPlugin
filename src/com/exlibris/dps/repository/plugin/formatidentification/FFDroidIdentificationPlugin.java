2816e639907cc60bdbfa26895fe353d234c8bb71
leefrank9527 <leefrank9527@gmail.com>
2023-08-30 16:25:01 +1200

    Updated the signature files and version numbers

--- src/com/exlibris/dps/repository/plugin/formatidentification/FFDroidIdentificationPlugin.java
+++ src/com/exlibris/dps/repository/plugin/formatidentification/FFDroidIdentificationPlugin.java
@@ -20,7 +20,7 @@ import com.exlibris.dps.sdk.formatidentification.FormatIdentificationResult;
 public class FFDroidIdentificationPlugin implements FormatIdentificationPlugin {
 	private static final String PLUGIN_VERSION_INIT_PARAM = "PLUGIN_VERSION_INIT_PARAM";
 	private static String AGENT_NAME = "REG_SA_DROID";
-	private static String AGENT_VERSION = "6.5.2";
+	private static String AGENT_VERSION = "6.6.1";
 	private static String REGISTRY_NAME = "PRONOM";
 	private long maxBytesToScan = -1;
 	private boolean inTag = false;
@@ -35,7 +35,7 @@ public class FFDroidIdentificationPlugin implements FormatIdentificationPlugin {
 	 * attached, update SIG_VERSION Or 2 different plugins with the same jar path
 	 * and jar name
 	 */
-	private static String SIG_VERSION = "_109";
+	private static String SIG_VERSION = "_112";
 
 	static {
 		Map map=null;
